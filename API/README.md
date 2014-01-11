99f96759d05a3bd818ee84da9806528d2c0a644b
Mikael Korpela <mikael@ihminen.org>
2014-01-11 01:40:39 +0000

    API connection

---
+++ API/README.md
@@ -0,0 +1,5 @@
+## DEV/DEMO API
+
+These files are just for dev/demo purposes.
+
+The idea is to export json files from MediaWiki using SemanticWiki extension.
--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,7 +1,61 @@
-angular.module("hostmapapp", ["leaflet-directive"]);
-function hostmapController($scope) {
-    
-    angular.extend($scope, {    
+/**
+ * Hitchwiki Hosts
+ */
+var APIpath = '../API/';
+
+var HWHosts = angular.module('hostmapapp', [
+        'leaflet-directive'
+    ]);
+
+
+/*
+HWHosts.factory('Markers', function($http, $log) {
+    var markers = [];
+
+    return {
+        get: function() {
+            $log.log("Markers->get()");
+
+            $http({method: 'GET', url: APIpath + 'hosts.json'}).
+                success(function(data, status, headers, config) {
+                    // this callback will be called asynchronously
+                    // when the response is available
+                    $log.log("Markers->get()->respond:");
+                    $log.log(data);
+                    if(data.results) {
+
+                        angular.forEach(data.results, function(place) {
+
+                            markers.push({
+                                lat:        place.printouts.Location[0].lat,
+                                lng:        place.printouts.Location[0].lon,
+                                layer:      'hosts',
+                                message:    '<p><a href="' + place.fullurl + '" target="_blank">' + place.fulltext + '</a></p>',
+                                focus:      true
+                            });
+
+                        });
+                    }
+
+                    return markers;
+
+                }).
+                error(function(data, status, headers, config) {
+                    // called asynchronously if an error occurs
+                    // or server returns response with an error status.
+                    markers = [];
+                    return markers;
+                });
+        }
+    };
+});
+*/
+
+
+
+HWHosts.controller('hostmapController', function($scope, $http, $log) { //Markers
+
+    angular.extend($scope, {
         hostmap: {
             lat: 38.71,
             lng: -9.1470,
@@ -30,7 +84,7 @@ function hostmapController($scope) {
                     }
                 }
             },
-            
+
             overlays: {
                 hosts: {
                     name: 'Hosts',
@@ -42,10 +96,50 @@ function hostmapController($scope) {
                 }
             }
         },
-        marker_list: [  ]
+        marker_list: []
     });
-    
-    $scope.addhost = function() { 
+
+
+    /**
+     * Fetch markers from the API
+     */
+    $scope.fetch = function() {
+        $http({method: 'GET', url: APIpath + 'hosts.json'}).
+            success(function(data, status, headers, config) {
+                // this callback will be called asynchronously
+                // when the response is available
+                $log.log("Markers->get()->respond:");
+                $log.log(data);
+                if(data.results) {
+
+                    angular.forEach(data.results, function(place) {
+
+                        $scope.marker_list.push({
+                            lat:        place.printouts.Location[0].lat,
+                            lng:        place.printouts.Location[0].lon,
+                            layer:      'hosts',
+                            message:    '<p><a href="' + place.fullurl + '" target="_blank">' + place.fulltext + '</a></p>',
+                            focus:      true
+                        });
+
+                    });
+                }
+
+                //return markers;
+
+            }).
+            error(function(data, status, headers, config) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+                $scope.marker_list = [];
+                //$log.error("Couldn't connecto to the API...");
+            });
+    };
+
+    $scope.fetch();
+
+
+    $scope.addhost = function() {
                 $scope.marker_list.push({
                     lng : $scope.hostmap.lng,
                     lat : $scope.hostmap.lat,
@@ -55,6 +149,6 @@ function hostmapController($scope) {
                     focus:true
                 });
     }
-    
 
-};
+
+});
